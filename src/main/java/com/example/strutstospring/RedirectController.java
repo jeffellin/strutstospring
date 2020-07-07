85d087fc9a7ed7033d364f54a1cd63b94569674e
jellin@vmware.com <jellin@vmware.com>
2020-07-07 13:31:45 -0400

    switch redirect config

--- src/main/java/com/example/strutstospring/RedirectController.java
+++ src/main/java/com/example/strutstospring/RedirectController.java
@@ -1,17 +1,28 @@
 package com.example.strutstospring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Controller
+@Configuration
 public class RedirectController {
 
-    @GetMapping("/")
-    public ModelAndView redirectWithUsingRedirectPrefix(ModelMap model) {
-        model.addAttribute("attribute", "redirectWithRedirectPrefix");
-        return new ModelAndView("redirect:/helloWorld.do", model);
+    @Bean
+    public WebMvcConfigurerAdapter forwardToIndex() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                // forward requests to /admin and /user to their index.html
+                registry.addViewController("/").setViewName(
+                        "forward:/helloWorld.do");
+
+            }
+        };
     }
 }
