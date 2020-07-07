10dfb3b193442658658214674d28b7cdf79abdab
jellin@vmware.com <jellin@vmware.com>
2020-07-07 13:19:12 -0400

    add redirect for main page

---
+++ src/main/java/com/example/strutstospring/RedirectController.java
@@ -0,0 +1,17 @@
+package com.example.strutstospring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class RedirectController {
+
+    @GetMapping("/")
+    public ModelAndView redirectWithUsingRedirectPrefix(ModelMap model) {
+        model.addAttribute("attribute", "redirectWithRedirectPrefix");
+        return new ModelAndView("redirect:/helloWorld.do", model);
+    }
+}
